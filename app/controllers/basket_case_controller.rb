1c1f3d6eeb0490897fe5911135e382d953d6ba4c
raconcepcion <roseanne.concepcion@gmail.com>
2024-02-18 21:23:49 +0800

    implements sort by amount & order by asc/desc

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -1,17 +1,18 @@
 class BasketCaseController < ApplicationController
   before_action :set_fruits
-  before_action :set_items
+  before_action :set_items, :set_order
 
   def index; end
 
   def order_by_fruit
     @list_items = @list_items.sort
-    @list_items = @list_items.reverse
+    @list_items = @list_items.reverse if params[:order].present? && params[:order] == "desc" 
     render 'basket_case/index'
   end
 
   def order_by_amount
-    # code here
+    @list_items = @list_items.sort_by(&:last)
+    @list_items = @list_items.reverse if params[:order].present? && params[:order] == "desc"
 
     render 'basket_case/index'
   end
@@ -29,5 +30,13 @@ class BasketCaseController < ApplicationController
 
     def set_items
       @list_items = @fruits
+    end
+
+    def set_order
+      if params[:order].present? && params[:order] == "asc"
+        @order = "desc" 
+      else 
+        @order = "asc" 
+      end
     end
 end
